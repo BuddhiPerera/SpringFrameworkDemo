5de2e2a8d9926cb85942517f7c4829a60320e5fd
Buddhi Perera <67018688+BuddhiPerera@users.noreply.github.com>
2022-11-12 07:25:18 +0530

    object injection using XML configs

--- src/main/java/demo/Doctor.java
+++ src/main/java/demo/Doctor.java
@@ -4,6 +4,9 @@ public class Doctor implements Staff{
 
     private String qualification;
 
+    // adding an object (for example purpose)
+    private Nurse nurse;
+
     public void assist(){
         System.out.println("Doctor is assisting");
     }
@@ -15,5 +18,13 @@ public class Doctor implements Staff{
 
     public void setQualification(String qualification) {
         this.qualification = qualification;
+    }
+
+    public Nurse getNurse() {
+        return nurse;
+    }
+
+    public void setNurse(Nurse nurse) {
+        this.nurse = nurse;
     }
 }
